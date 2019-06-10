2351a9c8041f39412357c4193a9297d238107e43
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-06-10 18:23:10 +0300

    try post data but fiasco

--- routes/recipes.js
+++ routes/recipes.js
@@ -7,10 +7,48 @@ router.get('/', (req, res) => {
   Dishes
     .findAll()
     .then(dishes => { 
-      console.log(dishes);
-      res.sendStatus(200);
+      res.send(dishes)
     })
-    .catch(error => console.log('Dishes error -> ', error))
+    .catch(error => console.log('Dishes error -> ', error));
+});
+
+router.post('/add', (req, res, next) => {
+  // const data = {
+  //   title: 'Mega Burger',
+  //   description: 'We take a boolka and meat kotleta',
+  //   imagePath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cheeseburger.jpg/1200px-Cheeseburger.jpg'
+  // };
+
+  // let { title, description, imagePath } = req.body;
+  let errors = [];
+  next(console.log(req))
+  
+  // if (!title) {
+  //   errors.push({ text: 'Please add a title' });
+  // }
+  // if (!description) {
+  //   errors.push({ text: 'Please add a description' });
+  // }
+  // if (!imagePath) {
+  //   errors.push({ text: 'Please add a image url' });
+  // }
+
+  // Check for errors
+  // if (error.length > 0) {
+
+  // } else {
+    //Insert into table
+  //   Dishes
+  //     .create({
+  //       title,
+  //       description,
+  //       imagePath
+  //     })
+  //     .then(dishes => res.redirect('/recipes'))
+  //     .catch( error => console.log('Dishes create error ->', error.message));
+  // }
+
+
 });
 
 module.exports = router;
